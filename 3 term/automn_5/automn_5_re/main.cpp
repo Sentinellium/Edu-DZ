f197829577dd06352e0829443c7799ff2996192e
Sentinellium <34355821+Sentinellium@users.noreply.github.com>
2018-12-19 10:33:21 +0300

    Update main.cpp

--- 3 term/automn_5/automn_5_re/main.cpp
+++ 3 term/automn_5/automn_5_re/main.cpp
@@ -51,7 +51,7 @@ void Sort(A* array, int n)
     int size = n;
     for(int i = 1; i < size; ++i){
       int j = i - 1;
-      int key = array[i];
+      A key = array[i];
       while(j >= 0 && array[j] > key){
         array[j+1] = array[j];
         --j;
